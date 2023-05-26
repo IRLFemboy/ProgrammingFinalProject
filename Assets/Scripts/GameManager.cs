446ee3274084dca530a63d8780be980ad19b9cf9
IRLFemboy <115759367+IRLFemboy@users.noreply.github.com>
2023-05-26 13:48:09 -0700

    Update GameManager.cs
    meow meow :)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI scoreDisplay;
-
+    
+    int waveNumber;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreDisplay = GameObject.Find(¨ScoreDisplay¨).GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
